9dd88bc887d517ab2187e1153b87ce2d77ef5e4a
OleksandrYanchuk <sashajanchuck13@gmail.com>
2023-11-09 21:14:40 +0200

    bot

--- auto_task.py
+++ auto_task.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python
-import os
 
 import schedule
 import time
@@ -26,41 +25,3 @@ def run_scheduled_job():
     while True:
         schedule.run_pending()
         time.sleep(1)
-
-# Use gunicorn to run the Flask app
-if __name__ == "__main__":
-    from gevent import monkey
-    monkey.patch_all()
-    import multiprocessing
-
-    # Number of worker processes
-    workers = multiprocessing.cpu_count() * 2 + 1
-
-    # Start the scheduled job in a separate thread
-    import threading
-    threading.Thread(target=run_scheduled_job).start()
-
-    # Run the Flask app with gunicorn
-    os.environ['MAGICK_NO_X11'] = '1'  # Set this if needed
-    gunicorn_options = {
-        'bind': '0.0.0.0:5000',  # Set the desired host and port
-        'workers': workers,
-        'worker_class': 'gevent',
-    }
-    from gunicorn.app.base import BaseApplication
-
-    class StandaloneApplication(BaseApplication):
-        def __init__(self, app, options=None):
-            self.options = options or {}
-            self.application = app
-            super(StandaloneApplication, self).__init__()
-
-        def load_config(self):
-            for key, value in self.options.items():
-                if key in self.cfg.settings and value is not None:
-                    self.cfg.set(key, value)
-
-        def load(self):
-            return self.application
-
-    StandaloneApplication(app, gunicorn_options).run()
